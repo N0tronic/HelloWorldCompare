82123d4dd3e91b781ee395956f7a82fe0e9fb330
N0tronic <florianmueller86@web.de>
2021-05-16 17:34:23 +0200

    add springboot hello world

---
+++ springboot/src/test/java/de/compare/springboot/SpringbootApplicationTests.java
@@ -0,0 +1,26 @@
+package de.compare.springboot;
+
+import org.hamcrest.core.IsNull;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.IsEqual.equalTo;
+
+@SpringBootTest
+class SpringbootApplicationTests {
+
+    @Autowired
+    RestController restController;
+
+    private final static String HELLO = "Hello World!";
+
+    @Test
+    void contextLoads() {
+        assertThat(restController, is(IsNull.notNullValue()));
+        assertThat(HELLO, equalTo(restController.hello()));
+    }
+
+}
